390252d2b32ea6531742783ae3fe78276529f57b
Ricky Ng-Adam <rngadam@gmail.com>
2018-12-14 22:41:51 +0800

    properly name track

--- sql/mobike-transform.sql
+++ sql/mobike-transform.sql
@@ -1,3 +1,6 @@
+drop materialized view if exists urbandata.mobike;
+
+-- 3m runtime
 create materialized view urbandata.mobike as
 	select 
 		cast(orderid as int),
@@ -5,10 +8,9 @@ create materialized view urbandata.mobike as
 		cast(userid as int),
 		cast(start_time as timestamp),
 		cast(start_location_x as float),
-		cast(start_location_y as float),
+		cast(start_location_y as float) ,
 		cast(end_time as timestamp),
 		cast(end_location_x as float),
 		cast(end_location_y as float),
-		regexp_split_to_array(track, '#')
-	from urbandata.raw_mobike 
-	limit 10;
+		regexp_split_to_array(track, '#') as track
+	from urbandata.raw_mobike;
